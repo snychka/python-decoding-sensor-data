5c7aad48c6c2f440d0098c705fb9268614079c07
Hugo Valle <hugovalle1@weber.edu>
2020-05-11 13:43:15 -0600

    Add 3 more tasks.

--- tasks.md
+++ tasks.md
@@ -5,6 +5,8 @@
   - [Module 1: Load Sensor Data From Files](#module-1-load-sensor-data-from-files)
     - [Task 1: Import os, glob, and csv](#task-1-import-os-glob-and-csv)
     - [Task 2: Create a Function to parse the data](#task-2-create-a-function-to-parse-the-data)
+    - [Task 3: Set Python File Management](#task-3-set-python-file-management)
+    - [Task 4: Read CSV Files](#task-4-read-csv-files)
   - [Module 2: Create a Class HomeData](#module-2-create-a-class-homedata)
   - [Module 3: Analyze Temperature Data](#module-3-analyze-temperature-data)
   - [Module 4: Analyze Humidity Data](#module-4-analyze-humidity-data)
@@ -21,30 +23,32 @@ Draft.
 
 [//]:# (@pytest.mark.test_load_data_function_module1)
 
-```python
-import os
-import glob
-import csv
-```
-
 The dataset for this project is stored in several CSV files found in the `dataset` folder. It represents the data collected from a device with multiple sensors. The records include measurements of temperature, humidity, energy consumption, and particle count in the air over a given area. The data is collected over a period of 24 hours.  
 
 To start, open the file called `load_data.py` in the `sensor` folder - the rest of the tasks in this module happen in this same file.
 
 At the top of the file create three import statements for `os`, `glob`, and `csv`. These libraries will allow us to work with a collection of files.
 
-### Task 2: Create a Function to parse the data 
+### Task 2: Create a Function to parse the data
 
 [//]:# (@pytest.mark.test_load_data_load_sensor_func_module1)
 
-```python
-def load_sensor_data():
-  # create a list to store data
-  sensor_data = []
-```
-
 Create a method called `load_sensor_data` that takes no arguments.
-In the body of the `load_sensor_data` function, create variable called `sensor_data` and set it as an empty `list`.
+In the body of the `load_sensor_data` function, create variable called `sensor_data` and set it as an empty `list` - the rest of the tasks in this module happen in this function.
+
+### Task 3: Set Python File Management
+
+[//]:# (@pytest.mark.test_load_data_sensor_files_module1)
+
+Below the `sensor_data` variable create another variable called `sensor_files` that is set to a call to the `glob.glob()` function.
+
+Pass `glob` function a single argument, a call to the `os.path.join()` function. In turn pass `os.path.join()` three arguments: `os.getcwd()`, `datasets`, and `*.csv`.
+
+The `datasets` argument corresponds to the folder with your sensor data. The data files are in `csv` format. You may open them and explore the content of the records in them.
+
+### Task 4: Read CSV Files
+
+[//]:# (@pytest.mark.test_load_data_load_sensor_func_module1)
 
 ## Module 2: Create a Class HomeData
 
--- tests/test_module1.py
+++ tests/test_module1.py
@@ -12,15 +12,15 @@ def test_load_data_import_module1(parse):
     load_data = parse("load_data")
     assert load_data.success, load_data.message
 
-    sys_import = load_data.imports("os")
-    assert sys_import, "Are you importing `os`?"
+    os_import = load_data.imports("os")
+    assert os_import, "Are you importing `os`?"
 
-    sys_import = load_data.imports("glob")
-    assert sys_import, "Are you importing `glob`?"
+    glob_import = load_data.imports("glob")
+    assert glob_import, "Are you importing `glob`?"
 
-    sys_import = load_data.imports("csv")
-    assert sys_import, "Are you importing `csv`?"
-
+    csv_import = load_data.imports("csv")
+    assert csv_import, "Are you importing `csv`?"
+    
 
 @pytest.mark.test_load_data_load_sensor_func_module1
 def test_load_data_load_sensor_func_module1(parse):
@@ -31,15 +31,15 @@ def test_load_data_load_sensor_func_module1(parse):
     load_data = parse("load_data")
     assert load_data.success, load_data.message
 
-    load_sensor_data = load_data.query("def load_sensor_data(): ??")
-    load_sensor_data_exists = load_sensor_data.exists()
+    load_sensor_data = load_data.defines("load_sensor_data")
     assert (
-        load_sensor_data_exists
+        load_sensor_data.exists()
     ), "Are you defining a function called `load_sensor_data` with the correct arguments?"
 
-    sensor_data = (
-        load_sensor_data.assign_()
-        .match(
+    # print(json.dumps(load_sensor_data.assign_().n, indent=2))
+    # assert False
+    
+    sensor_data = load_sensor_data.assign_().match(
             {
                 "type": "Assign",
                 "targets_0_type": "Name",
@@ -47,8 +47,214 @@ def test_load_data_load_sensor_func_module1(parse):
                 "value_type": "List",
             }
         )
-        .exists()
-    )
     assert (
         sensor_data
     ), "Are you creating a variable called `sensor_data` set equal to an empty list?"
+
+
+@pytest.mark.test_load_data_sensor_files_module1
+def test_load_data_sensor_files_module1(parse):
+
+    # def load_sensor_data():
+    #   ....
+    #   sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
+
+    load_data = parse("load_data")
+    assert load_data.success, load_data.message
+
+    # load_sensor_data = load_data.query("def load_sensor_data(): ??")
+    load_sensor_data = load_data.defines("load_sensor_data")
+    load_sensor_data_exists = load_sensor_data.exists()
+    assert (
+        load_sensor_data_exists
+    ), "Are you defining a function called `load_sensor_data` with the correct arguments?"
+
+    sensor_files = (
+        load_sensor_data.assign_()
+        .match(
+            {
+            "1_type": "Assign",
+            "1_targets_0_type": "Name",
+            "1_targets_0_id": "sensor_files",
+            "1_value_type": "Call",
+
+            "1_value_func_type": "Attribute",
+            "1_value_func_value_type": "Name",
+            "1_value_func_value_id": "glob",
+            "1_value_func_attr": "glob",
+            
+            "1_value_args_0_type": "Call",
+            "1_value_args_0_func_type": "Attribute",
+            "1_value_args_0_func_value_type": "Attribute",
+            "1_value_args_0_func_value_value_type": "Name",
+            "1_value_args_0_func_value_value_id": "os",
+            "1_value_args_0_func_value_attr": "path",
+            "1_value_args_0_func_attr": "join",
+            
+            "1_value_args_0_args_0_type": "Call",
+            "1_value_args_0_args_0_func_type": "Attribute",
+            "1_value_args_0_args_0_func_value_type": "Name",
+            "1_value_args_0_args_0_func_value_id": "os",
+            "1_value_args_0_args_0_func_attr": "getcwd",
+            "1_value_args_0_args_1_type": "Constant",
+            "1_value_args_0_args_1_value": "datasets",
+            "1_value_args_0_args_2_type": "Constant",
+            "1_value_args_0_args_2_value": "*.csv"
+            }
+        )
+        .exists()
+    )
+    assert (
+        sensor_files
+    ), "Are you creating a variable called `sensor_files` and assigning it glob.glob() and passing os.path.join()? Are you passing 3 values to os.path.join()?"
+
+
+
+@pytest.mark.test_load_data_sensor_files_query_module1
+def test_load_data_sensor_files_query_module1(parse):
+
+    # def load_sensor_data():
+    #   ....
+    #   sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
+
+    load_data = parse("load_data")
+    assert load_data.success, load_data.message
+
+    sensor_files = load_data.query("glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))")
+    sensor_files_exists = sensor_files.exists()
+    assert (
+        sensor_files_exists
+    ), "Are you including `glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))` in your code?"
+
+
+@pytest.mark.test_load_data_for_files_module1
+def test_load_data_for_files_module1(parse):
+
+    # def load_sensor_data():
+    #   ....
+    # for sensor_file in sensor_files:
+    #     with open(sensor_file) as data_file:
+    #         data_reader = csv.DictReader(data_file, delimiter=',')
+    #         for row in data_reader:
+    #             sensor_data.append(row)
+    # return sensor_data
+
+    load_data = parse("load_data")
+    assert load_data.success, load_data.message
+
+    # load_sensor_data = load_data.defines("load_sensor_data")
+    load_sensor_data = load_data.query("def load_sensor_data(): ??")
+    assert (
+        load_sensor_data.exists()
+    ), "Are you defining a function called `load_sensor_data` with the correct arguments?"
+    
+    # print(json.dumps(load_sensor_data.for_().n, indent=2))  # TODO Remove
+    # print(json.dumps(load_sensor_data.assign_().n, indent=2))
+    # print(json.dumps(load_sensor_data.returns_call().n, indent=2))
+    # print(json.dumps(load_sensor_data.assigns().n, indent=2))
+    # assert False
+
+    first_for_exists = (
+        load_sensor_data.for_()
+        .match(
+            {
+                "target_type": "Name",
+                "target_id": "sensor_file",
+                "iter_type": "Name",
+                "iter_id": "sensor_files",
+            }
+        )
+        .exists()
+    )
+    assert (
+        first_for_exists
+    ), 'Do you have a `for` loop, looping through `sensor_files`? Is the current loop value called `sensor_file`?'
+    
+    with_exists = (
+        load_sensor_data.for_()
+        .match(
+            {
+            "0_type": "With",
+            "0_items_0_type": "withitem",
+            "0_items_0_context_expr_type": "Call",
+            "0_items_0_context_expr_func_type": "Name",
+            "0_items_0_context_expr_func_id": "open",
+            "0_items_0_context_expr_args_0_type": "Name",
+            "0_items_0_context_expr_args_0_id": "sensor_file",
+            "0_items_0_optional_vars_type": "Name",
+            "0_items_0_optional_vars_id": "data_file"
+            }
+        )
+        .exists()
+    )
+    assert (
+        with_exists
+    ), "Do you have a call to `open` in your `with` code and are you passing `open` the correct argument?"
+
+    data_reader = (
+        load_sensor_data.for_()
+        .match(
+            {
+            "0_body_0_type": "Assign",
+            "0_body_0_targets_0_type": "Name",
+            "0_body_0_targets_0_id": "data_reader",
+            "0_body_0_value_type": "Call",
+            "0_body_0_value_func_type": "Attribute",
+            "0_body_0_value_func_value_type": "Name",
+            "0_body_0_value_func_value_id": "csv",
+            "0_body_0_value_func_attr": "DictReader",
+            "0_body_0_value_args_0_type": "Name",
+            "0_body_0_value_args_0_id": "data_file",
+            "0_body_0_value_keywords_0_type": "keyword",
+            "0_body_0_value_keywords_0_arg": "delimiter",
+            "0_body_0_value_keywords_0_value_type": "Constant",
+            "0_body_0_value_keywords_0_value_value": ",",
+            }
+        )
+        .exists()
+    )
+    assert (
+        data_reader
+    ), "Are you assigning `data_reader` the result of `csv.DictReader()` with the correct input argument and delimeter?"
+
+    second_for_exist = (
+        load_sensor_data.for_()
+        .match(
+            {
+            "0_body_1_type": "For",
+            "0_body_1_target_type": "Name",
+            "0_body_1_target_id": "row",
+            "0_body_1_iter_type": "Name",
+            "0_body_1_iter_id": "data_reader",
+            }
+        )
+        .exists()
+    )
+    assert (
+        second_for_exist
+    ), 'Do you have a second `for` loop, looping through `data_reader`? Is the current loop value called `row`?'
+    
+    sensor_data_append = (
+        load_sensor_data.for_()
+        .match(
+            {
+            "0_body_1_body_0_type": "Expr",
+            "0_body_1_body_0_value_type": "Call",
+            "0_body_1_body_0_value_func_type": "Attribute",
+            "0_body_1_body_0_value_func_value_type": "Name",
+            "0_body_1_body_0_value_func_value_id": "sensor_data",
+            "0_body_1_body_0_value_func_attr": "append",
+            "0_body_1_body_0_value_args_0_type": "Name",
+            "0_body_1_body_0_value_args_0_id": "row"
+            }
+        )
+        .exists()
+    )
+    assert (
+        sensor_data_append
+    ), 'Are you appending the `row` records to the `sensor_data` list?'
+
+    returns_load_sensor_data = load_sensor_data.returns("sensor_data")
+    assert (
+        returns_load_sensor_data
+    ), 'Are you returning `sensor_data` from `load_sensor_data` function?'
