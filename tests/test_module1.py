32fad465d6be7bf5e161f7693f24b1ae45033823
Hugo Valle <hugovalle1@weber.edu>
2020-05-13 11:58:48 -0600

    Update data set date column. Split task 4 into task 4 and 5

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 
+from sensor.load_data import load_sensor_data as lsd
 
 @pytest.mark.test_load_data_import_module1
 def test_load_data_import_module1(parse):
@@ -34,10 +35,7 @@ def test_load_data_load_sensor_func_module1(parse):
     load_sensor_data = load_data.defines("load_sensor_data")
     assert (
         load_sensor_data.exists()
-    ), "Are you defining a function called `load_sensor_data` with the correct arguments?"
-
-    # print(json.dumps(load_sensor_data.assign_().n, indent=2))
-    # assert False
+    ), "Are you defining a function called `load_sensor_data` with no input parameters?"
 
     sensor_data = load_sensor_data.assign_().match(
         {
@@ -135,6 +133,86 @@ def test_load_data_read_files_module1(parse):
     # for sensor_file in sensor_files:
     #     with open(sensor_file) as data_file:
     #         data_reader = csv.DictReader(data_file, delimiter=',')
+
+    load_data = parse("load_data")
+    assert load_data.success, load_data.message
+
+    # load_sensor_data = load_data.defines("load_sensor_data")
+    load_sensor_data = load_data.query("def load_sensor_data(): ??")
+    assert (
+        load_sensor_data.exists()
+    ), "Are you defining a function called `load_sensor_data` with the correct arguments?"
+
+    first_for_exists = (
+        load_sensor_data.for_()
+        .match(
+            {
+                "target_type": "Name",
+                "target_id": "sensor_file",
+                "iter_type": "Name",
+                "iter_id": "sensor_files",
+            }
+        )
+        .exists()
+    )
+    assert (
+        first_for_exists
+    ), 'Do you have a `for` loop, looping through `sensor_files`? Is the current loop value called `sensor_file`?'
+
+    with_exists = (
+        load_sensor_data.for_()
+        .match(
+            {
+                "0_type": "With",
+                "0_items_0_type": "withitem",
+                "0_items_0_context_expr_type": "Call",
+                "0_items_0_context_expr_func_type": "Name",
+                "0_items_0_context_expr_func_id": "open",
+                "0_items_0_context_expr_args_0_type": "Name",
+                "0_items_0_context_expr_args_0_id": "sensor_file",
+                "0_items_0_optional_vars_type": "Name",
+                "0_items_0_optional_vars_id": "data_file"
+            }
+        )
+        .exists()
+    )
+    assert (
+        with_exists
+    ), "Do you have a call to `open` in your `with` code and are you passing `open` the correct argument?"
+
+    data_reader = (
+        load_sensor_data.for_()
+        .match(
+            {
+                "0_body_0_type": "Assign",
+                "0_body_0_targets_0_type": "Name",
+                "0_body_0_targets_0_id": "data_reader",
+                "0_body_0_value_type": "Call",
+                "0_body_0_value_func_type": "Attribute",
+                "0_body_0_value_func_value_type": "Name",
+                "0_body_0_value_func_value_id": "csv",
+                "0_body_0_value_func_attr": "DictReader",
+                "0_body_0_value_args_0_type": "Name",
+                "0_body_0_value_args_0_id": "data_file",
+                "0_body_0_value_keywords_0_type": "keyword",
+                "0_body_0_value_keywords_0_arg": "delimiter",
+                "0_body_0_value_keywords_0_value_type": "Constant",
+                "0_body_0_value_keywords_0_value_value": ",",
+            }
+        )
+        .exists()
+    )
+    assert (
+        data_reader
+    ), "Are you assigning `data_reader` the result of `csv.DictReader()` with the correct input argument and delimeter?"
+
+
+
+@pytest.mark.test_load_data_load_recs_module1
+def test_load_data_load_recs_module1(parse):
+
+    # def load_sensor_data():
+    #   ....
     #         for row in data_reader:
     #             sensor_data.append(row)
     # return sensor_data
@@ -142,7 +220,6 @@ def test_load_data_read_files_module1(parse):
     load_data = parse("load_data")
     assert load_data.success, load_data.message
 
-    # load_sensor_data = load_data.defines("load_sensor_data")
     load_sensor_data = load_data.query("def load_sensor_data(): ??")
     assert (
         load_sensor_data.exists()
@@ -232,7 +309,9 @@ def test_load_data_read_files_module1(parse):
     )
     assert (
         second_for_exist
-    ), 'Do you have a second `for` loop, looping through `data_reader`? Is the current loop value called `row`?'
+    ), """Do you have a second `for` loop, looping through `data_reader`? 
+        Is the current loop value called `row`?
+        Is this loop part of the `with` block?"""
 
     sensor_data_append = (
         load_sensor_data.for_()
@@ -258,6 +337,15 @@ def test_load_data_read_files_module1(parse):
     assert (
         returns_load_sensor_data
     ), 'Are you returning `sensor_data` from `load_sensor_data` function?'
+
+    # Test Length of return value
+    # data_len = load_sensor_data()
+    data_len = lsd()
+    assert (
+        len(data_len) == 2000
+    ), """The length of your sensor_data list is incorrect. 
+        Are you scanning both data files?"""
+
 
 
 @pytest.mark.test_sensor_app_load_data_return_module1
@@ -290,7 +378,7 @@ def test_sensor_app_load_data_return_module1(parse):
     assert (
         data
     ), "Are you creating a variable called `data` set equal to `load_sensor_data()` function?"
-
+    
     print_data = sensor.calls().match(
         {
             "type": "Expr",
@@ -312,3 +400,4 @@ def test_sensor_app_load_data_return_module1(parse):
     assert (
         print_data
     ), "Are you calling `print()` and passing in `format()`? Are you passing 1 values to `format()` `len(data)`"
+    
