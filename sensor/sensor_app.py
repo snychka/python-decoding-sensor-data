b666426675f9dc106ecc0669edb7fcdf95eda5bb
Hugo Valle <hugovalle1@weber.edu>
2020-05-14 09:56:05 -0600

    Fix starting code

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,23 +1,19 @@
 # Runner script for all modules
-from load_data import load_sensor_data          # module 2
-from house_info import HouseInfo
-from datetime import date, datetime
 
+
+##############################
 # Do not remove these two lines
-# if you do, it will break the unittest
+# They are needed to validate your unittest
 recs = 0
 print("Sensor Data App")
+##############################
 
-recs = load_sensor_data()
-# print(f"Loaded records {len(data)}")
-print("Loaded records: [{}]".format(len(recs)))
+# Module 1 code here:
 
-house_info = HouseInfo(data)
-recs = house_info.get_data_by_area("id", area=1)
-print("House sensor records for area 1 = {}".format(
-       len(recs)))
+# Module 2 code here:
 
-rec_date = datetime.strptime("5/9/2020", "%m/%d/%Y")
-recs = house_info.get_data_by_date("id", rec_date)
-print("House sensor records for {} = {}".format(
-       rec_date.date(), len(recs)))
+# Module 3 code here:
+
+# Module 4 code here:
+
+# Module 5 code here:
