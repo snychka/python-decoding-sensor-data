4ba7cfe20bbf1eba42cbba759386f892af9f0de2
Stefan Nychka <stefannychka@gmail.com>
2020-05-26 12:30:02 -0600

    add to avoid conflicts
    will revert online to parent's master, then update and merge locally, and push up to my branch

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,34 +1,19 @@
-from house_info import HouseInfo
-from load_data import load_sensor_data 
-from datetime import datetime, date
-from temperature_info import TemperatureData
+# Runner script for all modules
 
-# don't remove
+
+##############################
+# Do not remove these two lines
+# They are needed to validate your unittest
 data = []
 print("Sensor Data App")
+##############################
 
-data = load_sensor_data()
-#data = []
-#print(f'Loaded records: [{data}]')
-print('Loaded records: {0}'.format(len(data)))
+# Module 2 code here:
 
-house_info = HouseInfo(data)
+# Module 3 code here:
 
-recs = house_info.get_data_by_area('id', rec_area=1)
-print('House sensor records for area 1 = {0}'.format(len(recs)))
-#rec_date = datetime.strptime("5/9/2020" , "%m/%d/%Y")
-#record_date = datetime.strptime("5/9/20" , "%m/%d/%y")
-test_date = datetime.strptime("5/9/20" , "%m/%d/%y")
-#recs = house_info.get_data_by_date("id", rec_date)
-#recs = house_info.get_data_by_date("id", record_date)
-recs = house_info.get_data_by_date("id", rec_date=test_date)
-print('House sensor records for 2020-05-09 = {0}'.format(len(recs)))
+# Module 4 code here:
 
-temperature_data = TemperatureData(data)
-recs = temperature_data.get_data_by_area(rec_area=1)
-print("\nHouse Temperature sensor records for area 1 = {}".format(len(recs)))
-print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+# Module 5 code here:
 
-recs = temperature_data.get_data_by_date(rec_date=test_date)
-print("\nHouse Temperature sensor records for area 1 = {}".format(len(recs)))
-print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+# Module 6 code here:
