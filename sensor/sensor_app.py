569aa5b12a50b38baba243ef1468cee69965cba2
Hugo Valle <hugovalle1@weber.edu>
2020-05-27 12:21:09 -0600

    Update typos and comments

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -8,13 +8,13 @@ data = []
 print("Sensor Data App")
 ##############################
 
-# Module 2 code here:
+# Module 1 code here:
 data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
+# Module 2 code here:
+
 # Module 3 code here:
 
 # Module 4 code here:
 
 # Module 5 code here:
-
-# Module 6 code here:
